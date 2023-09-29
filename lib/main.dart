5790a1115ac5fe6cc5c7dc6fc64422a4bdec96eb
Shubham Kumar <arjunraj582shu@gmail.com>
2023-09-29 19:40:18 +0530

    Second Animation
    This contains the code for second animation

--- lib/main.dart
+++ lib/main.dart
@@ -12,33 +12,125 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+extension on VoidCallback {
+  Future<void> delayed(Duration duration) => Future.delayed(duration, this);
+}
+
+enum CircleSide { left, right }
+
+extension ToPath on CircleSide {
+  Path toPath(Size size) {
+    final path = Path();
+    late Offset offset;
+    late bool clockwise;
+
+    switch (this) {
+      case CircleSide.left:
+        path.moveTo(size.width, 0);
+        offset = Offset(size.width, size.height);
+        clockwise = false;
+        break;
+      case CircleSide.right:
+        offset = Offset(0, size.height);
+        clockwise = true;
+        break;
+    }
+    path.arcToPoint(
+      offset,
+      radius: Radius.elliptical(size.width / 2, size.height / 2),
+      clockwise: clockwise,
+    );
+    path.close();
+    return path;
+  }
+}
+
+class HalfCircleClipper extends CustomClipper<Path> {
+  final CircleSide side;
+  const HalfCircleClipper({required this.side});
+  @override
+  Path getClip(Size size) => side.toPath(size);
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => true;
+}
+
+class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
   // This widget is the root of your application.
-  late AnimationController _controller;
-  late Animation<double> _animation;
+  late AnimationController _counterClockWiseRotationController;
+  late Animation<double> _counterClockWiseRotationAnimation;
+  late AnimationController _flipController;
+  late Animation<double> _flipanimation;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _controller = AnimationController(
+    //counterClockWiseRotationController
+    _counterClockWiseRotationController = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 5),
+      duration: const Duration(seconds: 1),
     );
-    _animation = Tween(
+    _counterClockWiseRotationAnimation = Tween(
       begin: 0.0,
-      end: 2 * pi,
-    ).animate(_controller);
-    _controller.repeat();
+      end: (-1 / 2) * pi,
+    ).animate(CurvedAnimation(
+        parent: _counterClockWiseRotationController, curve: Curves.bounceOut));
+
+    //flipController
+    _flipController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    _flipanimation = Tween(
+      begin: 0.0,
+      end: pi,
+    ).animate(CurvedAnimation(
+        parent: _counterClockWiseRotationController, curve: Curves.bounceOut));
+
+    //status listeners
+    _counterClockWiseRotationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _flipanimation = Tween(
+          begin: _flipanimation.value,
+          end: _flipanimation.value + pi,
+        ).animate(
+            CurvedAnimation(parent: _flipController, curve: Curves.bounceOut));
+
+        _flipController
+          ..reset()
+          ..forward();
+      }
+    });
+
+    _flipController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _counterClockWiseRotationAnimation = Tween(
+          begin: _counterClockWiseRotationAnimation.value,
+          end: _counterClockWiseRotationAnimation.value + (-1 / 2) * pi,
+        ).animate(CurvedAnimation(
+            parent: _counterClockWiseRotationController,
+            curve: Curves.bounceOut));
+
+        _counterClockWiseRotationController
+          ..reset()
+          ..forward();
+      }
+    });
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _counterClockWiseRotationController.dispose();
+    _flipController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    _counterClockWiseRotationController
+      ..reset()
+      ..forward.delayed(const Duration(seconds: 1));
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -48,29 +140,76 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
       ),
       darkTheme: ThemeData(brightness: Brightness.dark),
       home: Scaffold(
-          body: Center(
-        child: AnimatedBuilder(
-          animation: _controller,
-          builder: (context, child) {
-            return Transform(
-              alignment: Alignment.center,
-              transform: Matrix4.identity()..rotateY(_animation.value),
-              child: Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.black.withOpacity(0.5),
-                          spreadRadius: 5,
-                          blurRadius: 10,
-                          offset: const Offset(0, 3))
-                    ]),
-              ),
-            );
-          },
+          body: SafeArea(
+        child: Center(
+          child: AnimatedBuilder(
+            animation: _counterClockWiseRotationController,
+            builder: (context, child) {
+              return Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity()
+                  ..rotateZ(_counterClockWiseRotationAnimation.value),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    AnimatedBuilder(
+                      animation: _flipController,
+                      builder: (context, child) {
+                        return Transform(
+                          alignment: Alignment.centerRight,
+                          transform: Matrix4.identity()
+                            ..rotateY(_flipanimation.value),
+                          child: ClipPath(
+                            clipper:
+                                const HalfCircleClipper(side: CircleSide.left),
+                            child: Container(
+                              width: 100,
+                              height: 100,
+                              decoration:
+                                  BoxDecoration(color: Colors.blue, boxShadow: [
+                                BoxShadow(
+                                    color: Colors.black.withOpacity(0.5),
+                                    spreadRadius: 5,
+                                    blurRadius: 10,
+                                    offset: Offset(0, 3))
+                              ]),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    AnimatedBuilder(
+                      animation: _flipController,
+                      builder: (context, child) {
+                        return Transform(
+                          alignment: Alignment.centerLeft,
+                          transform: Matrix4.identity()
+                            ..rotateY(_flipanimation.value),
+                          child: ClipPath(
+                            clipper:
+                                const HalfCircleClipper(side: CircleSide.right),
+                            child: Container(
+                              width: 100,
+                              height: 100,
+                              decoration: BoxDecoration(
+                                  color: Colors.yellow,
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: Colors.black.withOpacity(0.5),
+                                        spreadRadius: 5,
+                                        blurRadius: 10,
+                                        offset: Offset(0, 3))
+                                  ]),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       )),
     );
